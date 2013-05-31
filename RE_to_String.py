116f5468867760e3fdf0f0c3e5367dc461a11c6c
xiada <hqxmail@gmail.com>
2013-05-31 15:43:30 +0800

    批量把smali文件夹中的所有0x某某t转成字符
    批量把smali文件夹中的所有0x某某t转成字符

--- RE_to_String.py
+++ RE_to_String.py
@@ -1,24 +1,34 @@
 import re
+import os
 
-smali = open(r'c:/AboutActivity.smali','r')
-smali_bak = open(r'c:/AboutActivity.txt','w')
-char_id_re = re.compile(r'0x(.+?)t')
-j = 0
-for i in smali:
-    j= j+1
-    char_id = char_id_re.findall(i)
+def walk_dir(dir,topdown=True):
+    for root,dirs,files in os.walk(dir,topdown):
+        for name in files:
+            smali_to_char(os.path.join(root,name))
+
+def smali_to_char(name):
+    smali = open(name,'r')
+    smali_bak = open(name+'bak','w')
+    char_id_re = re.compile(r'0x(.+?)t')
+    j = 0
+    for i in smali:
+        j= j+1
+        char_id = char_id_re.findall(i)
     
-    if len(char_id) != 0:
-        print j
-        smali_bak.write(chr(int(char_id[0],16)))
+        if len(char_id) != 0 and (r'sswitch' not in i):
+            smali_bak.write(chr(int(char_id[0],16)))
         
-    else:
-        if r'.end array-data' in i:
-            smali_bak.write('\n'+i)
         else:
-            smali_bak.write(i)
+            if r'.end array-data' in i:
+                smali_bak.write('\n'+i)
+            else:
+                smali_bak.write(i)
+    print 'Done'
 
-print 'Done'
+path = r'c:/smali'
+walk_dir(path)
+
+
         
     
     
