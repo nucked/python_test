b3cb50439b3e8eee7aa0cf261d2951a17ae9fbdf
xiada <hqxmail@gmail.com>
2013-06-13 15:56:12 +0800

    将utf-8字符转回0x某某t格式
    将utf-8字符转回0x某某t格式

---
+++ String_to_RE.py
@@ -0,0 +1,17 @@
+import re
+
+smali =  open(r'c:/3.smali','r')
+smali_bak = open(r'c:/4.smali','wb')
+mark = 0
+for word in smali:
+    j = word.decode('utf-8')
+    for i in j:
+            k = ord(i)
+            for x in unichr(k).encode('utf-8'):
+                smali_bak.write( hex(ord(x)) + 't\n')
+            
+
+smali.close()
+smali_bak.close()            
+        
+    
