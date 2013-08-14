2bbeb65e876e38dedabe723df95a6e0d2d0b0189
xiada <hqxmail@gmail.com>
2013-08-14 13:11:20 +0800

    增加判断中英文

--- String_to_RE.py
+++ String_to_RE.py
@@ -5,13 +5,15 @@ smali_bak = open(r'c:/4.smali','wb')
 mark = 0
 for word in smali:
     j = word.decode('utf-8')
-    for i in j:
-            k = ord(i)
-            for x in unichr(k).encode('utf-8'):
-                smali_bak.write( hex(ord(x)) + 't\n')
+    for i in j:        
+        k = ord(i)
+        if k<=128:
+            smali_bak.write(i)
+            continue
+        for x in unichr(k).encode('utf-8'):
+            smali_bak.write( hex(ord(x)) + 't\n')
+         
             
 
 smali.close()
-smali_bak.close()            
-        
-    
+smali_bak.close()   
